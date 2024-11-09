0780e760bf4c12018c6647410b73147b21d84e4b
Anas Anwar <anasanwar210@gmail.com>
2024-11-09 17:30:15 +0200

    Done

--- js/index.js
+++ js/index.js
@@ -5,21 +5,21 @@ let showQuote = document.getElementById("show-quote"),
 let englishQuotes = [
   {
     quote:
-      "People do not like to think. if one thinks, one must reach conclussions conclussions are not always pleasant.",
+      "People do not like to think if one thinks, one must reach conclussions conclussions are not always pleasant",
     author: "-- Helen Keller --",
   },
   {
-    quote: "Education without execution is extinction .",
+    quote: "Education without execution is extinction",
     author: "-- Bo Sanchez --",
   },
   {
     quote:
-      "Concentrate all your thoughts upon the work at hand the sun's rays do not burn untill brought to focus .",
+      "Concentrate all your thoughts upon the work at hand the sun's rays do not burn untill brought to focus",
     author: "-- Alexander Graham Bell --",
   },
   {
     quote:
-      "Tomorrow is my exam but i don't care because a single sheet of paper can't decide my future .",
+      "Tomorrow is my exam but i don't care because a single sheet of paper can't decide my future",
     author: "-- Thomas A. Edison --",
   },
   {
@@ -28,8 +28,13 @@ let englishQuotes = [
   },
   {
     quote:
-      "Whenever you read a book or have a conversation, the experience causes physical changes in your brain.",
+      "Whenever you read a book or have a conversation, the experience causes physical changes in your brain",
     author: "-- George Johnson --",
+  },
+  {
+    quote:
+      "سيب اللي يقول يقول، مش ماشيه بالأصول، العيش والملح داب، كله لأجل الوصول",
+    author: "-- عـبـسـلام --",
   },
 ];
 
@@ -39,10 +44,11 @@ function getRandomQuote() {
 
   do {
     randomQuotes = Math.floor(Math.random() * englishQuotes.length);
-    showQuote.style.height = "170px";
-    quote.innerHTML = `<p class="lead quote pt-2"><i class="fa-solid fa-quote-left"></i> ${englishQuotes[randomQuotes].quote} <i class="fa-solid fa-quote-right"></i></p>`;
-    author.innerHTML = `<p class="lead author pb-2"> ${englishQuotes[randomQuotes].author} </p>`;
-    console.log(randomQuotes);
+      showQuote.style.height = "260px";
+      showQuote.style.display = "block";
+      quote.innerHTML = `<p class="lead quote pt-4"><i class="fa-solid fa-quote-left"></i> ${englishQuotes[randomQuotes].quote} <i class="fa-solid fa-quote-right"></i></p>`;
+      author.innerHTML = `<p class="lead author pb-4 mb-0"> ${englishQuotes[randomQuotes].author} </p>`;
+      console.log(randomQuotes);
   } while (randomQuotes === lastNum);
   lastNum = randomQuotes;
 }
